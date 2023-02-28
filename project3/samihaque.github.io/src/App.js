a99c997a6f8b8a5519230d7a97af7047a1c68de0
Ubaydul Haque Sami <64384070+samihaque@users.noreply.github.com>
2023-02-27 23:25:29 -0500

    modified app js to say Hey I am starting with react

--- project3/samihaque.github.io/src/App.js
+++ project3/samihaque.github.io/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey!! I am starting with react
+          Hey!! This is sami
         </p>
         <a
           className="App-link"
