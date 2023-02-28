8853d4a202693a2d04118fffab294188de33fde5
Ubaydul Haque Sami <64384070+samihaque@users.noreply.github.com>
2023-02-27 23:48:45 -0500

    final commit, I will become a React pro

--- project3/samihaque.github.io/src/App.js
+++ project3/samihaque.github.io/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/samihaque"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check Me Out!
         </a>
       </header>
     </div>
